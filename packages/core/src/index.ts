49b15628e00b62563db8824c1b6b1b004f553301
Ángel Quiroz <aquirozdev@gmail.com>
2022-07-17 16:04:15 -0500

    core: export auth handlers
    Signed-off-by: Ángel Quiroz <aquirozdev@gmail.com>

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,7 @@
-export * from './lib/core';
+import loginHandler from 'infrastructure/api/auth/login';
+import registerHandler from 'infrastructure/api/auth/register';
+
+export default {
+  registerHandler,
+  loginHandler,
+};
