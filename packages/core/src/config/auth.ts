b1dc4bf34f117d68c507856ed681dbb742885006
Ángel Quiroz <aquirozdev@gmail.com>
2022-07-17 16:11:05 -0500

    Default config
    Signed-off-by: Ángel Quiroz <aquirozdev@gmail.com>

--- packages/core/src/config/auth.ts
+++ packages/core/src/config/auth.ts
@@ -1,2 +1,3 @@
-export const mongoUrl = process.env['MONGO_URI'] as string;
-export const mongoAuthDbName = process.env['MONGO_AUTH_DB_NAME'] as string;
+export const mongoUrl =
+  process.env['MONGO_URI'] || 'mongodb://localhost:27017/';
+export const mongoAuthDbName = process.env['MONGO_AUTH_DB_NAME'] || 'auth';
